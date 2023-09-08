82473210f72efd2a1476e5c9dcbeac0c85451812
Soldiamond <abiolavctr@gmail.com>
2023-09-08 00:41:43 -0700

    Print art

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,5 +10,5 @@ int main(void)
 	char myString[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myString);
-	return (0)
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	char myText[] = "With proper grammer, but the outcome is a piece of art.";
+
+	printf("%s\n", myText);
+	return (0);
+}
