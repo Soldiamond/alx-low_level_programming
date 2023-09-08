16d9ff94f37e40e6fd3260a09f5702e5ba78804b
Soldiamond <abiolavctr@gmail.com>
2023-09-07 17:20:19 -0700

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (sucrss)
+ */
+int main(){
+	char myString[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myString);
+	return (0);
+
