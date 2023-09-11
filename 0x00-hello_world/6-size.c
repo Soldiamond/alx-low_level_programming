8fd491f0e9a8aa64d96344aa29495d4c059a42d2
Soldiamond <abiolavctr@gmail.com>
2023-09-11 13:52:45 -0700

    Sizesss

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("size of a char: %lu byte(s)\n", sizeof(char));
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long init));
 	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
