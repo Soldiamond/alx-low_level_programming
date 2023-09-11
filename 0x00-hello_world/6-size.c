e4194067a1f38161e8c0a82f7e68b28e8893ccb6
Soldiamond <abiolavctr@gmail.com>
2023-09-11 14:22:20 -0700

    JUST WORK

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ * main -Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of a char: %lu bytes(s)\n", sizeof(char));
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long init));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
