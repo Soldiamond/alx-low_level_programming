efc8377ac7fb59e90173077fb626b2e2b4f81a7b
Soldiamond <abiolavctr@gmail.com>
2023-09-10 19:30:05 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ * /
+ * int main(void)
+ * {
+ * 	char ch;
+ *
+ * 	for (ch = 'a'; ch <= 'z'; ch++)
+ * 		putchar(ch);
+ * 	putchar('\n');
+ * 	return (0);
+ * }
