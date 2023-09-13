ecaafd72045f14508ea9a12233e8ebb03754c011
Soldiamond <abiolavctr@gmail.com>
2023-09-12 20:00:14 -0700

    alphabet printed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar('\n');
+}
