985f8314dc9880c53a181ee78b37df3865f227bc
Soldiamond <abiolavctr@gmail.com>
2023-09-15 20:04:44 -0700

    More Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print numbers between 0 to 14 incl.
+ *
+ * Return: Void.
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+				_putchar(i % 10 + '0');
+			i++;
+		}
+	_putchar('\n');
+	}
+}
+
