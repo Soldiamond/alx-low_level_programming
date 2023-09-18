c94303299656b18b50e0b6448072e81fdb7e7c61
Soldiamond <abiolavctr@gmail.com>
2023-09-18 11:27:23 -0700

    try pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - n reset to 98
+ * @n: An integer input pointer
+ * return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+#Pointers, Arrays and Strings
+alx-low_level_programming
+0x05-pointers_arrays_strings
